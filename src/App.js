f047f3365aad7b872bfc1a45129f9e5dd1cd96a2
leadcourt <leadcourt@gmail.com>
2025-07-10 02:32:16 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import EmailVerificationTool from "./EmailVerify.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <EmailVerificationTool />
+    </div>
+  )
+}
+
+export default App
